f56b4c2187ef79faebfc2a4760a868d8f75d4484
Yassine Bouazza <BOUAZZA_YASSINE@emsi-edu.ma>
2024-07-23 11:00:34 +0100

    MCRD-34:Structure Initiale du Projet

---
+++ src/main/java/ma/emsi/minicrm/MiniCrmApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.minicrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/emsi/minicrm/MiniCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.minicrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
