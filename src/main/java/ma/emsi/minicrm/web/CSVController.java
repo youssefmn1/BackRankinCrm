004eea68fa61f69907a30415e4274ce69d6861a7
Youssef Meziane <rcayoussefmz1rca@gmail.com>
2024-08-14 19:28:57 +0100

    fix RestControllerLead and csvController crm

--- src/main/java/ma/emsi/minicrm/web/CSVController.java
+++ src/main/java/ma/emsi/minicrm/web/CSVController.java
@@ -3,17 +3,17 @@ package ma.emsi.minicrm.web;
 import com.opencsv.exceptions.CsvException;
 import ma.emsi.minicrm.services.CSVService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
 
-@Controller
+@RestController
 @RequestMapping("/api/csv")
 public class CSVController {
 
@@ -21,23 +21,22 @@ public class CSVController {
     private CSVService csvService;
 
     @PostMapping("/import")
-    public RedirectView importCSV(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+    public ResponseEntity<String> importCSV(@RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
-            redirectAttributes.addFlashAttribute("message", "Please upload a CSV file.");
-            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
-            return new RedirectView("/leads");
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Please upload a CSV file.");
         }
 
         try {
             csvService.importCSV(file);
-            redirectAttributes.addFlashAttribute("message", "CSV file imported successfully.");
-            redirectAttributes.addFlashAttribute("alertClass", "alert-info");
-
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body("CSV file imported successfully.");
         } catch (IOException | CsvException e) {
-            redirectAttributes.addFlashAttribute("message", "An error occurred while importing the CSV file.");
-            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
-
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An error occurred while importing the CSV file.");
         }
-        return new RedirectView("/leads");
     }
 }
